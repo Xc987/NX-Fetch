cfb5dd242ca5518dc44f4e29a6260fb8fd61b851
Ricards Lacis <ricards.lacis18@gmail.com>
2025-03-02 22:24:22 +0200

    print system info as nickname, user, os and cfw versions

--- source/main.c
+++ source/main.c
@@ -7,7 +7,8 @@ int main(int argc, char **argv) {
     padConfigureInput(1, HidNpadStyleSet_NpadStandard);
     PadState pad;
     padInitializeDefault(&pad);
-    printf("\n\e[38;5;33m ######## \e[38;5;196m#######\n");
+
+    printf("\n\e[38;5;33m   ######## \e[38;5;196m#######\n");
     printf("\e[38;5;33m  ##      # \e[38;5;196m#########\n");
     printf("\e[38;5;33m #        # \e[38;5;196m##########\n");
     printf("\e[38;5;33m#   ###   # \e[38;5;196m###########\n");
@@ -27,6 +28,81 @@ int main(int argc, char **argv) {
     printf("\e[38;5;33m #        # \e[38;5;196m########## \n");
     printf("\e[38;5;33m  ##      # \e[38;5;196m#########\n");
     printf("\e[38;5;33m   ######## \e[38;5;196m#######\n");
+
+    // Variables
+    bool userflag = false;
+
+    // Console nickname
+    setsysInitialize();
+    SetSysDeviceNickName nickname;
+    memset(&nickname, 0, sizeof(nickname));
+    setsysGetDeviceNickname(&nickname);
+    setsysExit();
+
+    // Preselected user
+    Result rc = accountInitialize(AccountServiceType_Application);
+    AccountProfileBase profileBase;
+    if (R_SUCCEEDED(rc)) {
+        AccountUid userID;
+        rc = accountGetPreselectedUser(&userID);
+        if (R_SUCCEEDED(rc)) {
+            AccountProfile profile;
+            rc = accountGetProfile(&profile, userID);
+            if (R_SUCCEEDED(rc)) {
+                rc = accountProfileGet(&profile, NULL, &profileBase);
+                userflag = true;
+                accountProfileClose(&profile);
+            }
+        }
+    }
+    accountExit();
+
+    // HOS version
+    setsysInitialize();
+    SetSysFirmwareVersion fw_version;
+    setsysGetFirmwareVersion(&fw_version);
+    setsysExit();
+
+    // AMS version
+    char versionString[64];
+    u64 version;
+    splInitialize();
+    if (R_SUCCEEDED(splGetConfig((SplConfigItem)65000, &version))) {
+        int major = (version >> 56) & 0xFF;
+        int minor = (version >> 48) & 0xFF;
+        int patch = (version >> 40) & 0xFF;
+        snprintf(versionString, sizeof(versionString), "%d.%d.%d", major, minor, patch);
+        if (R_SUCCEEDED(splGetConfig((SplConfigItem)65007, &version))) {
+            strncat(versionString, version ? "|E" : "|S", sizeof(versionString) - strlen(versionString) - 1);
+        }
+    } else {
+        snprintf(versionString, sizeof(versionString), "Unknown");
+    }
+    splExit();
+
+    // Display info
+    int len = strlen(nickname.nickname);
+    int len2 = strlen(profileBase.nickname);
+    printf(CONSOLE_ESC(0m)CONSOLE_ESC(2;27H));
+    if (userflag) {
+        printf("%s@%s", profileBase.nickname, nickname.nickname);
+        printf(CONSOLE_ESC(3;27H));
+        for (int i = 0; i < (len + len2) + 1; i++) {
+            printf("-");
+        }
+    } else {
+        printf("%s", nickname.nickname);
+        printf(CONSOLE_ESC(3;27H));
+        for (int i = 0; i < len ; i++) {
+            printf("-");
+        }
+    }
+    printf(CONSOLE_ESC(4;27H));
+    printf("OS: Horizon OS %d.%d.%d", fw_version.major, fw_version.minor, fw_version.micro);
+
+    printf(CONSOLE_ESC(5;27H));
+    printf("CFW: Atmosphere %s", versionString);
+    
     while (appletMainLoop()) {
         padUpdate(&pad);
         u64 kDown = padGetButtonsDown(&pad);
